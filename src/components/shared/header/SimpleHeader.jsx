7d31310d404f3714f0cd2ee1e3a93e580f164b34
Mh Mitas <mahfuzulmitas@gmail.com>
2024-07-17 08:00:30 +0600

    commit - vercel.json file add and deploy

--- src/components/shared/header/SimpleHeader.jsx
+++ src/components/shared/header/SimpleHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const SimpleHeader = () => {
+    const { user } = useAuth()
+
     return (
         <section className='my-container py-6'>
-            <h3 className='text-3xl font-semibold mb-2'>Welcome, <span className='text-primary'>Mahim</span></h3>
+            <h3 className='text-3xl font-semibold mb-2'>Welcome, <span className='text-primary'>{user?.name}</span></h3>
             <p className='text-lg'>Access & manage your account and transactions efficiently.</p>
         </section>
     );
